cb140775c7da623b3560fc998ac5ea12c98d99ce
Leon Mwandiringa <leonmwandiringa@gmail.com>
2022-04-13 15:47:19 +0200

    this change

--- api-gateway-rest-api/stage.tf
+++ api-gateway-rest-api/stage.tf
@@ -1,9 +1,9 @@
 resource "aws_api_gateway_stage" "default" {
   deployment_id        = aws_api_gateway_deployment.default.id
   rest_api_id          = aws_api_gateway_rest_api.default.id
-  stage_name           = module.this.stage
+  stage_name           = module.default.stage
   xray_tracing_enabled = var.xray_tracing_enabled
-  tags                 = module.this.tags
+  tags                 = module.default.tags
 
   variables = {
     vpc_link_id = local.vpc_link_enabled ? aws_api_gateway_vpc_link.default.id : null
