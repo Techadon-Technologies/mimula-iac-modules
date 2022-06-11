72f982d39c5c4919d95682a5970ab12c2e1f9cc9
Leon Mwandiringa <leon@basalt.co>
2022-06-11 13:55:57 +0200

    network updates with nats and eip

--- network/route-table.tf
+++ network/route-table.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "default_private" {
 #nats etc
 resource "aws_route" "private_nat_gateway" {
   count                  = var.nats_enabled ? var.az_count : 0
-  route_table_id         = element(aws_route_table.default_public.*.id, count.index)
+  route_table_id         = element(aws_route_table.default_private.*.id, count.index)
   nat_gateway_id         = element(aws_nat_gateway.default.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
   depends_on = [
