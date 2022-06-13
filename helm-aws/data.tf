ae9c7a40a97aacece0f8f0d44334f544cb583fa0
Leon Mwandiringa <leon@basalt.co>
2022-06-13 17:37:28 +0200

    helm aws

--- helm-aws/data.tf
+++ helm-aws/data.tf
@@ -7,6 +7,6 @@ data "terraform_remote_state" "eks" {
   config = {
     bucket  = var.tfstate_bucket
     key     = var.tfstate_key
-    region  = var.tags.region
+    region  = var.tags.Region
   }
 }
