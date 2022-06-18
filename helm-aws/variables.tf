c9886eb588aef25668edce0ceb17160193d32dd4
Leon Mwandiringa <leon@basalt.co>
2022-06-19 01:51:39 +0200

    assumerole with identity role support

--- helm-aws/variables.tf
+++ helm-aws/variables.tf
@@ -33,9 +33,9 @@ variable "resources_list" {
     cleanup_on_fail = string
     for_update = string
     namespace = string
-    set_list = object({
+    set_list = list(object({
         name = string
         value = string
-    })
+    }))
   }))
 }
