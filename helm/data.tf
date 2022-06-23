8b3535e8da58c6363a24115307683f45a8f630f7
Leon Mwandiringa <leon@basalt.co>
2022-06-23 14:29:22 +0200

    helm package update

--- helm/data.tf
+++ helm/data.tf
@@ -1,6 +1,7 @@
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_id
 }
-data "aws_eks_node_groups" "node_group" {
+data "aws_eks_node_group" "node_group" {
   name = var.cluster_id
+  node_group_name = var.cluster_id
 }
