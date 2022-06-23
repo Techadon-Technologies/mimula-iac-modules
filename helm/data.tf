ba04e6ba71022b2153f93900fe413c3e3c70bf08
Leon Mwandiringa <leon@basalt.co>
2022-06-23 14:34:46 +0200

    helm package update

--- helm/data.tf
+++ helm/data.tf
@@ -1,7 +1,3 @@
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_id
 }
-data "aws_eks_node_group" "node_group" {
-  name = var.cluster_id
-  node_group_name = var.cluster_id
-}
--- kubernetes-resources/provider.tf
+++ kubernetes-resources/provider.tf
@@ -1,10 +1,6 @@
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_id
 }
-data "aws_eks_node_groups" "node_group" {
-  name = var.cluster_id
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
