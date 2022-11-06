778077de4a149bc018943ab4b850e2c04cc4723e
Leon Mwandiringa <leonmwandiringa@gmail.com>
2022-11-06 20:48:08 +0200

    refactor az type

--- memcached/subnet-group.tf
+++ memcached/subnet-group.tf
@@ -1,10 +1,4 @@
 resource "aws_elasticache_subnet_group" "default" {
   name       = "${var.cluster_name}-cache-subnet"
   subnet_ids = var.subnets
-  tags = merge(
-    var.glogal_tags,
-    {
-        Name = "${var.cluster_name}-cache-subnet"
-    }
-  )
 }
