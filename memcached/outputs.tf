50240b4a5bafe4c1395bd45838bf37d72d98544c
Leon Mwandiringa <leonmwandiringa@gmail.com>
2022-11-06 21:05:29 +0200

    sg removal deprecated

--- memcached/outputs.tf
+++ memcached/outputs.tf
@@ -4,16 +4,16 @@ output "cluster_subnet_groups_name" {
 }
 
 output "cluster_arn" {
-  value = aws_elasticache_subnet_group.default.name
+  value = aws_elasticache_cluster.default.name
   description = "cluster arn"
 }
 
 output "cluster_address" {
-  value = aws_elasticache_subnet_group.default.cluster_address
+  value = aws_elasticache_cluster.default.cluster_address
   description = "cluster cluster_address"
 }
 
 output "cluster_endpoint" {
-  value = aws_elasticache_subnet_group.default.configuration_endpoint
+  value = aws_elasticache_cluster.default.configuration_endpoint
   description = "cluster configuration_endpoint"
 }
